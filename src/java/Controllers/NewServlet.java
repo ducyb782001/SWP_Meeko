03b1c0f3643488e0856825b0e1bea17e1d16f598
ducndt <ducyb782001@gmail.com>
2023-05-24 23:33:04 +0700

    feat: add html css of login register jsp

--- src/java/Controllers/NewServlet.java
+++ src/java/Controllers/NewServlet.java
@@ -53,7 +53,7 @@ public class NewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        request.getRequestDispatcher("/views/MainPage.jsp").forward(request, response);  
+        request.getRequestDispatcher("/views/Login.jsp").forward(request, response);  
     } 
 
     /** 
